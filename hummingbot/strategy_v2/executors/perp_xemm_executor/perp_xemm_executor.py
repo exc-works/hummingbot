1da0ab0396a74c4b31d18ce9f3afa5bdb6271f8f
Diego Brando <dio97699@gmail.com>
2026-06-16 17:03:46 +0800

    getfee add positon_action

--- hummingbot/strategy_v2/executors/perp_xemm_executor/perp_xemm_executor.py
+++ hummingbot/strategy_v2/executors/perp_xemm_executor/perp_xemm_executor.py
@@ -356,6 +356,7 @@ class PerpXEMMExecutor(ExecutorBase):
             quote_currency=trading_pair.split("-")[1],
             order_type=order_type,
             order_side=TradeType.BUY if is_buy else TradeType.SELL,
+            position_action=PositionAction.OPEN,
             amount=order_amount,
             price=self._taker_result_price,
             is_maker=order_type.is_limit_type(),
